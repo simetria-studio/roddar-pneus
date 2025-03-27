d468aff29c19bd2eaea53adc96bc832300ce5d20
simetria-studio <fecruz2022@gmail.com>
2025-03-27 09:24:23 -0300

    a

--- lib/class/api_config.dart
+++ lib/class/api_config.dart
@@ -1,4 +1,4 @@
 class ApiConfig {
-  static const String apiUrl = 'http://10.0.2.2:8000/api';
-  // static const String apiUrl = 'https://app.x-erp.com.br/api';
+  // static const String apiUrl = 'http://10.0.2.2:8000/api';
+  static const String apiUrl = 'https://app.x-erp.com.br/api';
 }
